e7d4f30155dbdaa801093a9f004bbfe9c7c8e193
Xi Zhang <xizhang@oasislabs.com>
2025-07-27 21:24:34 +0200

    Add submitted state

--- src/app.jsx
+++ src/app.jsx
@@ -5,6 +5,7 @@ import "./app.css";
 function App() {
   const [showPrayer, setShowPrayer] = createSignal(false);
   const [showAddress, setShowAddress] = createSignal(false);
+  const [isSubmitted, setIsSubmitted] = createSignal(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,75 +19,85 @@ function App() {
       });
       
       const result = await res.json();
-      alert(result.message);
+      setIsSubmitted(true);
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <form name="prayer-request-form" onSubmit={handleSubmit}>
-      <h2>Hallo!</h2>
+    <>
+      {!isSubmitted() ? (
+        <form name="prayer-request-form" onSubmit={handleSubmit}>
+          <h2>Hallo!</h2>
 
-      <p><strong>What is on your heart?</strong></p>
-      
-      <label>
-        <input
-          type="checkbox"
-          onInput={(e) => setShowPrayer(e.currentTarget.checked)}
-        /> I'd like to share a prayer request 🥺
-      </label>
-      <br />
-      <label>
-        <input
-          type="checkbox"
-          onInput={(e) => setShowAddress(e.currentTarget.checked)}
-        /> I'd love a note or postcard 💌
-      </label>
-      <br />
+          <p><strong>What is on your heart?</strong></p>
 
-      <br />
-      <label for="name">Your Name</label>
-      <br />
-      <input
-        type="text"
-        id="name"
-        name="name"
-        placeholder="First and Last"
-        required
-      />
-      <br />
-
-      {showPrayer() && (
-        <>
-          <label for="prayer">Prayer Request</label>
+          <label>
+            <input
+              type="checkbox"
+              onInput={(e) => setShowPrayer(e.currentTarget.checked)}
+            /> I'd like to share a prayer request 🥺
+          </label>
           <br />
-          <textarea
-            id="prayer"
-            name="prayer"
-            rows="4"
-            placeholder="How can I petition on your behalf?"
-          ></textarea>
+          <label>
+            <input
+              type="checkbox"
+              onInput={(e) => setShowAddress(e.currentTarget.checked)}
+            /> I'd love a note or postcard 💌
+          </label>
           <br />
-        </>
+
+          <br />
+          <label for="name">Your Name</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="First and Last"
+            required
+          />
+          <br />
+
+          {showPrayer() && (
+            <>
+              <label for="prayer">Prayer Request</label>
+              <br />
+              <textarea
+                id="prayer"
+                name="prayer"
+                rows="4"
+                placeholder="How can I petition on your behalf?"
+              ></textarea>
+              <br />
+            </>
+          )}
+
+          {showAddress() && (
+            <>
+              <label for="address">Mailing Address</label>
+              <br />
+              <textarea
+                id="address"
+                name="address"
+                rows="4"
+                placeholder="Street, City, State, Zip (VERY IMPORTANT!), and Country"
+              ></textarea>
+              <br />
+            </>
+          )}
+
+          <button type="submit">Send</button>
+        </form>
+      ) : (
+        <form>
+          <h2>Thank you! 🙏</h2>
+          <p>Your submission has been received.</p>
+          <button onClick={() => setIsSubmitted(false)}>Submit Another</button>
+        </form>
       )}
-
-      {showAddress() && (
-        <>
-          <label for="address">Mailing Address</label>
-          <br />
-          <textarea
-            id="address"
-            name="address"
-            rows="4"
-            placeholder="Street, City, State, Zip (VERY IMPORTANT!), and Country"
-          ></textarea>
-          <br />
-        </>
-      )}
-
-      <button type="submit">Send</button>
-    </form>
+    </>
   );
 }
 
